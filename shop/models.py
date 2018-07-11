cd5f41ce55a8dfa7bb0922f99ed730518aa0525f
DaeSung <anseotjd12@gmail.com>
2018-07-11 14:46:53 +0900

    Category에 대한 admin 적용

--- shop/models.py
+++ shop/models.py
@@ -7,12 +7,18 @@ class Category(models.Model):
     icon = models.ImageField(blank=True)
     is_public = models.BooleanField(default=False, db_index=True)
 
+    def __str__(self):
+        return self.name
+
 class Shop(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, db_index=True)
     desc = models.TextField(blank=True)
     photo = models.ImageField(blank=True)
     is_public = models.BooleanField(default=False, db_index=True)
+
+    def __str__(self):
+        return self.name
 
 class Item(models.Model):
     name = models.CharField(max_length=100, db_index=True)
@@ -22,10 +28,16 @@ class Item(models.Model):
     rating = models.SmallIntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
     is_public = models.BooleanField(default=False, db_index=True)
 
+    def __str__(self):
+        return self.name
+
 class Review(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.author
 
 #class Order(models.Model):
 #    pass
