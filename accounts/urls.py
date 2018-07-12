cf517a758d697e177c640b4a853c1856c1f4e3db
DaeSung <anseotjd12@gmail.com>
2018-07-12 16:39:15 +0900

    login, logout, profile, signup 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from django.contrib.auth.views import login
-from django.urls import path, include
+from django.contrib.auth.views import login, logout
+from django.urls import path, include, reverse_lazy
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('signup/',views.signup, name='signup'),
     path('login/',login, name='login', kwargs={
         'template_name': 'accounts/login_form.html',
-        
     }),
+    path('logout/', logout, name='logout', kwargs={
+        'template_name': 'accounts/logout_form.html',
+        'next_page': reverse_lazy('root'),
+    }),
+    path('profile/', views.profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+User = get_user_model()
 
-# Create your views here.
+signup = CreateView.as_view(model=User, form_class=UserCreationForm,
+    template_name='accounts/signup_form.html',
+    success_url= reverse_lazy('root'))
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
